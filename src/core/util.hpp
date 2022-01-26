c32e9153f5ec737393079b06333020e119de1139
s.cobb.2022 <sjcobb2003@gmail.com>
2022-01-26 20:21:20 +0800

    added pretty function alternative

--- src/core/util.hpp
+++ src/core/util.hpp
@@ -7,6 +7,12 @@
 #include <functional>
 #include <string>
 
+
+// <-- MACROS -->
+#if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
 namespace rush{
 
     namespace util{
