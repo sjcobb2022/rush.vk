652e15e8ac8c6d0faff7484f7bd78bd068bbdfdf
s.cobb.2022 <sjcobb2003@gmail.com>
2022-01-26 20:19:40 +0800

    transfer ECS into ECS folder + deleted extensions

---
+++ src/ECS/components/relationship.hpp
@@ -0,0 +1,15 @@
+
+#include <entt/entity/registry.hpp>
+
+#include <cstddef>
+
+namespace rush{
+
+    struct c_Relationship{
+        std::size_t children{};
+        entt::entity first{entt::null};
+        entt::entity prev{entt::null};
+        entt::entity next{entt::null};
+        entt::entity parent{entt::null};
+    }
+}
