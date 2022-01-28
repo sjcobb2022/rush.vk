8a829fae6700ef1051ac2a7d08aa2d44ea59f6be
s.cobb.2022 <sjcobb2003@gmail.com>
2022-01-28 16:05:53 +0800

    added runtime view creation

--- src/ECS/scene.hpp
+++ src/ECS/scene.hpp
@@ -1,29 +1,69 @@
+#pragma once
+#include <entt/entt.hpp>
+#include <spdlog/spdlog.h>
 
-#include <entt/entity/registry.hpp>
+#include "components/transform.hpp"
+
+// #include "group_storage.hpp"
 
 #include <functional>
 #include <vector>
+#include <deque>
+#include <array>
+#include <memory>
 
-namespace rush{
+struct ignore_t{};
 
-    class Scene{
+namespace rush
+{
 
-        public:
-            Scene();
-            ~Scene();
+    class Scene
+    {
 
-            template<typename Component , typename... Other, typename... Exclude>
-            void createRuntimeView(
+    public:
+        Scene();
+        ~Scene();
 
-                //TODO: create a alias for the runtime view to make things a bit easier for users
+        template <typename... Components, typename... Exclude>
+        void createRuntimeViewIterator(std::function<void(entt::entity)> &&func, entt::exclude_t<Exclude...> = {})
+        {
+            renderViews.push_back(
+                [&]()
+                {
+                    auto view = entt::runtime_view{};
 
-                std::function<void(entt::runtime_view)>&&func, 
-                entt::exclude_t<Exclude...> = {} /*define exclude as empty since not necesssary*/
-                
-                );
-            
-        private:
-            entt::registry m_Registry;
-            std::vector<std::function<void(entt::runtime_view)>> renderViews;
+                    // fancy lambda that took way too long to make.
+                    // basically just a for loop over all the components but its funcy because of typename
+                    ([&]()
+                     { view.iterate(m_Registry.storage<Components>()); }(),
+                     ...);
+                    ([&]()
+                     { view.exclude(m_Registry.storage<Exclude>()); }(),
+                     ...);
+
+                    // parrallel mode?
+                    //  std::for_each(std::execution::par_unseq, view.begin(), view.end(), [&view](auto entity) {
+                    //      func(entity);
+                    //  });
+
+                    for (auto ent : view)
+                    {
+                        func(ent);
+                    }
+                });
+        }
+
+        void flushViewIterators()
+        {
+            for (auto it = renderViews.rbegin(); it != renderViews.rend(); it++)
+            {
+                (*it)(); // call functors
+            }
+        }
+
+        entt::registry m_Registry;
+
+    private:
+        std::deque<std::function<void()>> renderViews;
     };
 }
