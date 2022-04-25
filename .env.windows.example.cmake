f1e65e50cdd7e74d68c93289d54f196b85a9cb56
Samuel Cobb <sjcobb2003@gmail.com>
2022-04-25 10:54:29 +0800

    more understandable example

--- .env.windows.example.cmake
+++ .env.windows.example.cmake
@@ -1,11 +1,15 @@
 
-set(VULKAN_SDK_PATH  "YOUR PATH TO VULKAN")
-# set(ENTT_PATH "libs/entt")
-set(GLFW_PATH "YOUR PATH TO GLFW")
-# set(ENTT_PATH "libs/entt")
-set(GLM_PATH "YOUR PATH TO GLM")
-# set(ENTT_PATH "libs/entt")
-set(SPDLOG_PATH "YOUR PATH TO SPDLOG")
-# set(ENTT_PATH "libs/entt")
-set(ENTT_PATH "YOUR PATH TO ENTT")
-# set(ENTT_PATH "libs/entt")
+# set(VULKAN_SDK_PATH  "YOUR PATH TO VULKAN")
+set(VULKAN_PATH "C:/VulkanSDK/{version}")
+
+# set(GLFW_PATH "YOUR PATH TO GLFW")
+set(GLFW_PATH "libs/glfw")
+
+# set(GLM_PATH "YOUR PATH TO GLM")
+set(GLM_PATH "libs/glm")
+
+# set(SPDLOG_PATH "YOUR PATH TO SPDLOG")
+set(SPDLOG_PATH "libs/spdlog")
+
+# set(ENTT_PATH "YOUR PATH TO ENTT")
+set(ENTT_PATH "libs/entt")
