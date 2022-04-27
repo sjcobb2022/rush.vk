d8ccfacc67b3657297c760fae33a0caedd4f5663
sjcobb2022 <sjcobb2003@gmail.com>
2022-04-27 16:26:19 +0800

    removed debugging messages

--- src/core/scene.hpp
+++ src/core/scene.hpp
@@ -68,7 +68,6 @@ namespace rush
 
         template<typename... Components>
         void emplace(entt::entity ent, Components... Args){
-            spdlog::debug("creating N entities");
             (m_Registry.emplace<Components>(ent, Args), ...);
             return;
         }
@@ -103,8 +102,7 @@ namespace rush
             }
             else
             {
-                // spdlog::debug("Each Function :: fails valid_operator");
-                throw std::runtime_error("Cannot create lambda-in-lamda view, this causes a segfault");
+                throw std::runtime_error("Cannot create lambda-in-lamda view, this causes a segfault for some reason");
                 // runtimeViewFunctors.push_back([&f, this]()
                 //                               { each(std::forward<F>(f)); });
             }
@@ -123,12 +121,11 @@ namespace rush
         {
             if constexpr (has_valid_operator_v<Func>)
             {
-                spdlog::debug("Each Function :: has_parenthesis");
                 Each<decltype(&Func::operator())>::each(std::forward<Func>(func), m_Registry);
             }
             else
             {
-                spdlog::debug("Each Function :: fails has_parenthesis");
+                spdlog::debug("Internal :: fails has_parenthesis");
                 each(std::forward<Func>(func));
                 throw std::runtime_error("Cannot create lambda-in-lamda view, this causes a segfault");
             }
@@ -139,7 +136,7 @@ namespace rush
         std::deque<std::function<void()>> runtimeViewFunctors;
 
         //<-------- DO NOT TOUCH :: WILL CAUSE SEGFAULTS -------->
-        // Credits to Nikki93 for help implementing her templating fuckery
+        // Credits to Nikki93 for helping me with her templating fuckery
         // https://gist.github.com/nikki93/3cee41b34af3cefe5733d9a5fc502876#file-entity-hh-L72
 
         template <typename T>
@@ -151,7 +148,6 @@ namespace rush
             template <typename F>
             static void each(F &&f, entt::registry &reg)
             {
-                spdlog::debug("Each struct :: only entity");
                 reg.each(std::forward<F>(f));
             }
         };
@@ -162,8 +158,6 @@ namespace rush
             template <typename F>
             static void each(F &&f, entt::registry &reg)
             {
-                spdlog::debug("Each struct :: referenced components");
-
                 reg.view<T, Ts...>().each([&](entt::entity ent, T &t, Ts &...ts)
                                           { f(ent, t, ts...); });
             }
@@ -174,7 +168,6 @@ namespace rush
             template <typename F>
             static void each(F &&f, entt::registry &reg)
             {
-                spdlog::debug("Each struct :: pointer components");
                 reg.view<T, Ts...>().each([&](entt::entity ent, T &t, Ts &...ts)
                                           { f(ent, &t, &ts...); });
             }
@@ -183,7 +176,6 @@ namespace rush
         template <typename R, typename... Args>
         inline void each(R (&f)(Args...))
         {
-            spdlog::debug("Each function :: pass on lambda");
             each([&](Args... args)
                  { f(std::forward<Args>(args)...); });
         }
