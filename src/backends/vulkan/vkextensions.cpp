8573f7d31b0d030dba3c39dc93982f516bc85dfb
sjcobb2022 <sjcobb2003@gmail.com>
2022-04-27 11:58:22 +0800

    VK instance, extension, layers in builder

---
+++ src/backends/vulkan/vkextensions.cpp
@@ -0,0 +1,109 @@
+#include "vkextensions.hpp"
+
+namespace rush
+{
+
+    SystemInfo& get_system_info()
+    {
+        // GET AVAILABLE LAYERS
+        bool validation_layers_available = false;
+        bool debug_utils_available = false;
+        uint32_t layerCount = 0;
+
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+        std::vector<VkLayerProperties> layers(layerCount);
+        if (vkEnumerateInstanceLayerProperties(&layerCount, layers.data()) != VK_SUCCESS)
+        {
+            layers.clear();
+            spdlog::critical("Failed to get any layers, aborting");
+            throw std::runtime_error("Failed to get any layers, aborting");
+        }
+
+        spdlog::info("Available layers: ");
+        for (auto &layer : layers)
+        {
+            spdlog::info("\t{}", layer.layerName);
+            if (strcmp(layer.layerName, "VK_LAYER_KHRONOS_validation") == 0)
+            {
+                validation_layers_available = true;
+            }
+        }
+
+        // GET AVAILABLE EXTENSIONS
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        spdlog::info("Available extensions: ");
+        for (const auto &extension : extensions)
+        {
+            spdlog::info("\t{}", extension.extensionName);
+            if (strcmp(extension.extensionName, VK_EXT_DEBUG_UTILS_EXTENSION_NAME) == 0)
+            {
+                debug_utils_available = true;
+            }
+        }
+
+        for (auto &layer : layers)
+        {
+            std::vector<VkExtensionProperties> layer_extensions;
+            uint32_t layerExtensionCount = 0;
+            vkEnumerateInstanceExtensionProperties(layer.layerName, &layerExtensionCount, nullptr);
+            layer_extensions.resize(layerExtensionCount);
+
+            if (vkEnumerateInstanceExtensionProperties(layer.layerName, &layerExtensionCount, layer_extensions.data()) == VK_SUCCESS)
+            {
+                extensions.insert(extensions.end(), layer_extensions.begin(), layer_extensions.end()); // add trailing extensions
+            }
+
+            for (auto &ext : layer_extensions)
+            {
+                if (strcmp(ext.extensionName, VK_EXT_DEBUG_UTILS_EXTENSION_NAME) == 0)
+                {
+                    debug_utils_available = true;
+                }
+            }
+        };
+
+        SystemInfo info = {};
+        info.layers = layers;
+        info.extensions = extensions;
+        info.validation_layers_available = validation_layers_available;
+        info.debug_utils_available = debug_utils_available;
+
+        return info;
+    }
+
+    bool check_extension_supported(
+        std::vector<VkExtensionProperties> const &available_extensions,
+        const char *extension_name)
+    {
+        if (!extension_name)
+            return false;
+
+        for (const auto &extension_properties : available_extensions)
+        {
+            if (strcmp(extension_name, extension_properties.extensionName) == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool check_extensions_supported(
+        std::vector<VkExtensionProperties> const &available_extensions,
+        std::vector<const char *> const &extension_names)
+    {
+        bool all_found = true;
+        for (const auto &extension_name : extension_names)
+        {
+            bool found = check_extension_supported(available_extensions, extension_name);
+            if (!found)
+                all_found = false;
+        }
+        return all_found;
+    }
+}
--- src/backends/vulkan/vkinstance.hpp
+++ src/backends/vulkan/vkinstance.hpp
@@ -3,6 +3,7 @@
 // libs
 #include <vulkan/vulkan.h>
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 // std
 #include <vector>
@@ -16,45 +17,50 @@
 const char *to_string_message_severity(VkDebugUtilsMessageSeverityFlagBitsEXT s);
 const char *to_string_message_type(VkDebugUtilsMessageTypeFlagsEXT s);
 
-inline VKAPI_ATTR VkBool32 VKAPI_CALL default_debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                                                             VkDebugUtilsMessageTypeFlagsEXT messageType,
-                                                             const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-                                                             void *)
-{
-    const char *s_message_type = to_string_message_type(messageType);
-
-    switch (messageSeverity)
-    {
-    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
-        spdlog::debug(" {} :: {} ", s_message_type, pCallbackData->pMessage);
-        break;
-    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
-        spdlog::info(" {} :: {} ", s_message_type, pCallbackData->pMessage);
-        break;
-    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
-        spdlog::warn(" {} :: {} ", s_message_type, pCallbackData->pMessage);
-        break;
-    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
-        spdlog::error(" {} :: {} ", s_message_type, pCallbackData->pMessage);
-    default:
-        // only other case is VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT
-        spdlog::critical(" {} :: {} ", s_message_type, pCallbackData->pMessage);
-        break;
-    }
-
-    return VK_FALSE;
-}
-
 namespace rush
 {
+
+    inline VKAPI_ATTR VkBool32 VKAPI_CALL default_debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                                 VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                                 const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+                                                                 void *)
+    {
+        const char *s_message_type = to_string_message_type(messageType);
+
+        switch (messageSeverity)
+        {
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            spdlog::debug(" {} :: {} ", s_message_type, pCallbackData->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            spdlog::info(" {} :: {} ", s_message_type, pCallbackData->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            spdlog::warn(" {} :: {} ", s_message_type, pCallbackData->pMessage);
+            break;
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            spdlog::error(" {} :: {} ", s_message_type, pCallbackData->pMessage);
+        default:
+            // only other case is VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT
+            spdlog::critical(" {} :: {} ", s_message_type, pCallbackData->pMessage);
+            break;
+        }
+
+        return VK_FALSE;
+    }
 
     struct Instance
     {
         VkInstance instance = VK_NULL_HANDLE;
         VkDebugUtilsMessengerEXT debug_messenger = VK_NULL_HANDLE;
         VkAllocationCallbacks *allocation_callbacks = VK_NULL_HANDLE;
-        PFN_vkGetInstanceProcAddr fp_vkGetInstanceProcAddr = nullptr;
-        PFN_vkGetDeviceProcAddr fp_vkGetDeviceProcAddr = nullptr;
+
+    private:
+        bool supports_properties2_ext = false;
+        uint32_t instance_version = VK_API_VERSION_1_0;
+        uint32_t api_version = VK_API_VERSION_1_0;
+
+        friend class InstanceBuilder;
     };
 
     class InstanceBuilder
@@ -62,7 +68,7 @@ namespace rush
     public:
         explicit InstanceBuilder();
 
-        Instance build() const;
+        Instance &build() const;
 
         InstanceBuilder &set_app_name(const char *app_name);
         InstanceBuilder &set_engine_name(const char *engine_name);
@@ -73,16 +79,16 @@ namespace rush
         InstanceBuilder &enable_layer(const char *layer);
         InstanceBuilder &enable_extension(const char *ext);
 
-        InstanceBuilder &request_validation_layer(const char *val_layer);
-        InstanceBuilder &enable_validation_layer(const char *val_layer);
+        InstanceBuilder &request_validation_layers(const bool enable_layers);
+        InstanceBuilder &enable_validation_layers(const bool enable_layers);
 
         /**
          * @brief Set the debug callback function
-         * 
-         * @param callback 
-         * @return InstanceBuilder& 
+         *
+         * @param callback
+         * @return InstanceBuilder&
          */
-        InstanceBuilder& set_debug_callback(PFN_vkDebugUtilsMessengerCallbackEXT callback);
+        InstanceBuilder &set_debug_callback(PFN_vkDebugUtilsMessengerCallbackEXT callback);
 
         InstanceBuilder &add_validation_feature_enable(VkValidationFeatureEnableEXT enable);
 
@@ -94,7 +100,7 @@ namespace rush
             const char *application_name;
             uint32_t application_v;
             const char *engine_name;
-            uint32_t engine_b;
+            uint32_t engine_v;
             uint32_t api_v;
 
             std::vector<const char *> layers;
@@ -107,9 +113,18 @@ namespace rush
 
             VkAllocationCallbacks *allocation_callbacks = VK_NULL_HANDLE;
 
-            bool request_validation_laters = false;
+            bool request_validation_layers = false;
             bool enable_validation_layers = false;
             bool use_debug_messenger = false;
+
+            VkDebugUtilsMessageSeverityFlagsEXT debug_message_severity =
+                VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+            VkDebugUtilsMessageTypeFlagsEXT debug_message_type =
+                VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 
             PFN_vkGetInstanceProcAddr fp_vkGetInstanceProcAddr = nullptr;
 
